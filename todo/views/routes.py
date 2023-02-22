7f2434b6e4086d6cb79b1752f4b2f1cede0c5afd
Harikesha Suresh <32443198+Abishtu@users.noreply.github.com>
2023-02-22 01:47:05 +0000

    fixing return status code assertion error

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -16,7 +16,7 @@ def get_todos():
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }])
+    }]), 201
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
@@ -28,7 +28,7 @@ def get_todo(id):
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    })
+    }), 201
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
@@ -40,7 +40,7 @@ def create_todo():
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    })
+    }), 201
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
@@ -52,7 +52,7 @@ def update_todo(id):
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    })
+    }), 201
 
 @api.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
@@ -64,4 +64,4 @@ def delete_todo(id):
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    })
+    }), 201
